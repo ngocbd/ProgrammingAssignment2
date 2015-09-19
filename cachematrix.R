3e6ea1529c5ad9ae08a4141aab4e2232d750823b
daica <buidinhngoc.aiti@gmail.com>
2015-09-19 10:48:19 +0700

    add some comment for explain

--- cachematrix.R
+++ cachematrix.R
@@ -2,15 +2,18 @@
 ## functions do
 
 ## Write a short comment describing this function
+## These functions are to proxy to solve function
+## usage : cacheSolve ( makeCacheMatrix ( theMatrix ) )
 
+## Caches the passed in matrix
 makeCacheMatrix <- function(x = matrix()) {
 	 matrix <- NULL
   set <- function(y) {
     x <<- y # x hold new value
     matrix <<- NULL # reset 
   }
-  get <- function() x
-  setsolve <- function(solve) matrix <<- solve
+  get <- function() x # return  x
+  setsolve <- function(solve) matrix <<- solve 
   getsolve <- function() matrix
   list(set      = set, 
        get      = get,
@@ -20,7 +23,8 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-
+## checks if object passed in makeCacheMatrix object already has the cached inverse then returns if it has
+## computes solve on matrix of makeCacheMatrix, set it to makeCacheMatrix and return it
 cacheSolve <- function(x, ...) {
   
   matrix <- x$getsolve()
@@ -34,29 +38,3 @@ cacheSolve <- function(x, ...) {
   matrix
 }
 
-makeVector <- function(x = numeric()) {
-        m <- NULL
-        set <- function(y) {
-                x <<- y
-                m <<- NULL
-        }
-        get <- function() x
-        setmean <- function(mean) m <<- mean
-        getmean <- function() m
-        list(set = set, get = get,
-             setmean = setmean,
-             getmean = getmean)
-}
-
-
-cachemean <- function(x, ...) {
-        m <- x$getmean()
-        if(!is.null(m)) {
-                message("getting cached data")
-                return(m)
-        }
-        data <- x$get()
-        m <- mean(data, ...)
-        x$setmean(m)
-        m
-}
